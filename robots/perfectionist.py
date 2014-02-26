fcc02eccd77add71dceccf213162d1174bfe7a29
QAston <qaston@gmail.com>
2014-02-26 22:22:49 +0100

    robots/perfectionist.py Troche lepiej niedziała niż wcześniej

--- robots/perfectionist.py
+++ robots/perfectionist.py
@@ -1,9 +1,12 @@
 #
-# This robot assumes that everything is perfect - movement, sensors,speed of gps
+# This robot assumes that movement is perfect
+# uses distance sensor to see obstacles
+# doesn't use GPS
 #
 from defines import *
 from robot_controller import RobotController
 import math
+import random
 
 class PRC(RobotController):
 
@@ -24,14 +27,13 @@ class PRC(RobotController):
         self.distance_to_obstacle = None
 
         self.map = {}
-        self.commands = []#[PRC._StateDecideNext(self)]
-        self.command = self._EmptyCommand(self)
+        self.times_visited = {}
+        self.commands = [self._CheckCurrent(self)] # first command to run
+        self.command = PRC._EmptyCommand(self)
+        self.map[self.current_position] = self.current_field
 
     def act(self):
-        # decide what to do
-
-
-
+        # run next command from list
         act = None
         while act is None:
             last_result = self.command.done()
@@ -39,12 +41,7 @@ class PRC(RobotController):
                 if len(self.commands) != 0:
                     self.command = self.commands.pop(0)
                 else:
-                    self.commands.append(self._CheckDistanceCommand(self))
-                    self.commands.append(self._MoveDistanceCommand(self, 1.0))
-                    self.commands.append(self._TurnAngleCommand(self, math.pi*0.5))
-                    self.commands.append(self._CheckDistanceCommand(self))
-                    self.commands.append(self._CheckFieldCommand(self))
-                    self.commands.append(self._CheckGoal(self))
+                    self.command = None
 
             act = self.command.act()
 
@@ -64,99 +61,164 @@ class PRC(RobotController):
     def get_discrete_position(self):
         return int(self.current_position[0]+0.5) , int(self.current_position[1]+0.5)
 
-    # class _MultiCommand(object):
-    #     def __init__(self):
-    #         self.commands = []
-    #         self.command = PRC._EmptyCommand(None)
+    def get_forward_position(self):
+        pos = self.get_discrete_position()
 
-    #     def act(self):
-    #         # decide what to do
-    #         act = None
-    #         while act is None:
-    #             last_result = self.command.done()
-    #             if last_result is not None:
-    #                 if len(self.commands) != 0:
-    #                     self.command = self.commands.pop(0)
-    #                 else:
-    #                     self.plan()
+        return get_front(pos, self.current_angle)
 
-    #                     self.command = self.commands.pop(0)
-    #             act = self.command.act()
-
-    #         return act
-
-    #     def plan(self):
-    #         pass
-
-    #     def done(self):
-    #         pass
-
-    # class _State(_MultiCommand):
-    #     def __init__(self):
-    #         PRC._MultiCommand.__init__(self)
-    #         self.planned = False
-
-    #     def plan(self):
-    #         if not self.planned:
-    #             self.planned = True
-    #             self.actions()
-    #         else:
-    #             self.commands.append(PRC._EmptyCommand(None))
-
-    #     def actions(self):
-    #         pass
-
-    #     def done(self):
-    #         return True if self.planned and len(self.commands) == 0 else None
-
-    # class _StateDecideNext(_State):
-    #     def __init__(self, controller):
-    #         PRC._State.__init__(self)
-    #         self.controller = controller
-
-    #     def actions(self):
-    #         self.commands.append(PRC._CheckFieldCommand(self.controller))
-    #         self.commands.append(PRC._CheckGoal(self.controller))
-    #         self.commands.append(PRC._ChangeStateCommand(self.controller, PRC._StateMoveNext(self.controller)))
-
-
-    # class _StateMoveNext(_State):
-    #     def __init__(self, controller):
-    #         PRC._State.__init__(self)
-    #         self.controller = controller
-
-    #     def actions(self):
-    #         self.commands.append(PRC._MoveDistanceCommand(self.controller, 1.0))
-    #         self.commands.append(PRC._ChangeStateCommand(self.controller, PRC._StateDecideNext(self.controller)))
-
-    # class _ChangeStateCommand(object):
-    #     def __init__(self, controller, state):
-    #         self.controller = controller
-    #         self.state = state
-
-    #     def act(self):
-    #         return None
-
-    #     def done(self):
-    #         self.controller.commands.append(self.state);
-    #         return True
-
-
-    # class _CommandSequence(_MultiCommand):
-    #     def __init__(commands):
-    #         self.commands = commands
-    #         self.finished = False
-
-    #     def done(self):
-    #         return True if self.finished else None
-
-    #     def plan(self):
-    #         self.finished = True
+    def get_neighbour_positions(self):
+        #""" returns [front, left, back, right] """"
+        curr = self.get_discrete_position()
+        ret = []
+        angle = self.current_angle;
+        ret.append((curr, get_front(curr, angle)))
+        for i in range(3):
+            angle += math.pi * 0.5
+            angle %= 2*math.pi
+            ret.append((curr, get_front(curr, angle)))
+        return ret
 
 
     # commands
+    # subclasses of PRC because of the way simulator loads classes
     # act - returns action to do for a robot, can be None
     # done - returns None if action isn't finished yet, other things when finished
+
+    # select what field we should go next to
+    class _SelectNext(object):
+        def __init__(self, controller):
+            self.controller = controller
+
+        def act(self):
+            controller = self.controller
+            c = controller.commands
+
+            x_disc, y_disc = controller.get_discrete_position()
+
+            # mark field as known
+            controller.map[(x_disc, y_disc)] = controller.current_field
+
+            forward = controller.get_forward_position()
+
+            if controller.times_visited[(x_disc, y_disc)] == 0:
+                c.append(PRC._MoveNext(controller))
+            else:
+                neighbours = controller.get_neighbour_positions()
+                left, back, right = (neighbours[i+1] not in controller.times_visited for i in range(3))
+
+                # choose optimal rotations
+                if left and right:
+                    c.append(PRC._TurnAngleCommand(controller, 0.5 *math.pi))
+                    c.append(PRC._CheckDistanceCommand(self.controller))
+                    c.append(PRC._MarkNotVisitedField(self.controller))
+                    c.append(PRC._TurnAngleCommand(controller, 0.5 *math.pi))
+                    if (back):
+                        c.append(PRC._CheckDistanceCommand(self.controller))
+                        c.append(PRC._MarkNotVisitedField(self.controller))
+                    c.append(PRC._TurnAngleCommand(controller, 0.5 *math.pi))
+                    c.append(PRC._CheckDistanceCommand(self.controller))
+                    c.append(PRC._MarkNotVisitedField(self.controller))
+                elif left:
+                    c.append(PRC._TurnAngleCommand(controller, 0.5 *math.pi))
+                    c.append(PRC._CheckDistanceCommand(self.controller))
+                    c.append(PRC._MarkNotVisitedField(self.controller))
+                elif right or back:
+                    c.append(PRC._TurnAngleCommand(controller, -0.5 *math.pi))
+                    if right:
+                        c.append(PRC._CheckDistanceCommand(self.controller))
+                        c.append(PRC._MarkNotVisitedField(self.controller))
+                    if back:
+                        c.append(PRC._TurnAngleCommand(controller, -0.5 *math.pi))
+                        c.append(PRC._CheckDistanceCommand(self.controller))
+                        c.append(PRC._MarkNotVisitedField(self.controller))
+                else:
+                    visits = [controller.times_visited[neigh] for neigh in neighbours]
+                    min_visits = min(visits)
+                    if min_visits == neighbours[0]:
+                        pass
+                    elif min_visits == neighbours[1]:
+                        c.append(PRC._TurnAngleCommand(controller, 0.5 *math.pi))
+                    elif min_visits == neighbours[3]:
+                        c.append(PRC._TurnAngleCommand(controller, -0.5 *math.pi))
+                    else:
+                        c.append(PRC._TurnAngleCommand(controller, math.pi))
+
+                    c.append(PRC._MoveNext(controller))
+                    return None
+
+                # try selecting direction once again
+                c.append(PRC._SelectNext(controller))
+                return None
+
+
+        def done(self):
+            return True
+
+    class _MoveNext(object):
+        def __init__(self, controller):
+            self.controller = controller
+        def act(self):
+            c = self.controller.commands
+            c.append(PRC._MoveDistanceCommand(self.controller, 1.0))
+            c.append(PRC._CheckCurrent(self.controller))
+            return None
+
+        def done(self):
+            return True
+
+    # called once per visit
+    class _CheckCurrent(object):
+        def __init__(self, controller):
+            self.controller = controller
+
+        def act(self):
+            controller = self.controller
+            c = controller.commands
+            x_disc, y_disc = controller.get_discrete_position()
+            # get field type
+            if (x_disc, y_disc) not in controller.map:
+                c.append(PRC._CheckFieldCommand(self.controller))
+                c.append(PRC._CheckGoal(self.controller))
+            else:
+                controller.current_field = controller.map[(x_disc, y_disc)]
+
+            # mark visited
+            if (x_disc, y_disc) not in controller.times_visited:
+                controller.times_visited[(x_disc, y_disc)] = 1
+            else:
+                controller.times_visited[(x_disc, y_disc)] += 1
+
+            forward = controller.get_forward_position()
+
+            if forward not in controller.times_visited:
+                c.append(PRC._CheckDistanceCommand(self.controller))
+                c.append(PRC._MarkNotVisitedField(self.controller))
+
+            c.append(PRC._SelectNext(self.controller))
+            return None
+
+        def done(self):
+            return True
+
+    class _MarkNotVisitedField(object):
+        def __init__(self, controller):
+            self.controller = controller
+
+        def act(self):
+            controller = self.controller
+            forward = controller.get_forward_position()
+
+
+            # mark as wall
+            if controller.distance_to_obstacle < 0.9:
+                controller.times_visited[forward] = 65536
+            # mark as not visited
+            else:
+                controller.times_visited[forward] = 0
+
+        def done(self):
+            return True
+
 
     # caution: TICK_MOVE may be not precise enough
     class _MoveDistanceCommand(object):
@@ -181,15 +243,15 @@ class PRC(RobotController):
             self.controller = controller
 
         def act(self):
-            return [TURN, int(angle / TICK_ROTATE)]
+            return [TURN, int(self.angle / TICK_ROTATE)]
 
         def done(self):
-            self.controller.current_angle = (self.controller.current_angle + angle) % (2*math.pi)
+            self.controller.current_angle = (self.controller.current_angle + self.angle) % (2*math.pi)
             return True
 
     class _CheckDistanceCommand(object):
         def __init__(self, controller):
-            self.controller = controller.last_sonar_read
+            self.controller = controller
 
         def act(self):
             return [SENSE_SONAR]
@@ -239,3 +301,7 @@ class PRC(RobotController):
 
         def act(self):
             return None
+
+def get_front(pos, angle):
+    dist = 1.0;
+    return int(pos[0] + 0.5 + dist * math.cos(angle)) , int(pos[1] + 0.5 + dist * math.sin(angle))
