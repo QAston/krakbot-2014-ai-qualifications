fbf53512f1e5571d898f7d7a07b592f5450c8ba6
QAston <qaston@gmail.com>
2014-02-28 11:40:41 +0100

    robots/perfectionist: add a function for calculating min number of samples needed for desired error margin and certainty

--- robots/perfectionist.py
+++ robots/perfectionist.py
@@ -126,12 +126,6 @@ class PRC(RobotController):
                 neighbours = controller.get_neighbour_positions()
                 front, left, back, right = (neighbours[i] not in controller.times_visited for i in range(4))
 
-                if front:
-                    c.append(PRC._CheckDistanceCommand(self.controller))
-                    c.append(PRC._MarkNotVisitedField(self.controller))
-                    c.append(PRC._SelectNext(controller))
-                    return None
-
                 # choose optimal rotations
                 if left and right:
                     c.append(PRC._TurnAngleCommand(controller, 0.5 *math.pi))
@@ -188,7 +182,6 @@ class PRC(RobotController):
                 c.append(PRC._SelectNext(controller))
                 return None
 
-
         def done(self):
             return True
 
@@ -238,6 +231,35 @@ class PRC(RobotController):
         def done(self):
             return True
 
+    # class _CheckHints(object):
+    #     def __init__(self, controller):
+    #         self.controller = controller
+
+    #     def act(self):
+    #         controller = self.controller
+    #         c = controller.commands
+
+    #         if controller.current_field[0] == MAP_SPECIAL_OPTIMAL:
+    #             c.append(PRC._CheckDistanceCommand(self.controller))
+    #             c.append(PRC._CheckDistanceCommand(self.controller))
+
+    #         c.append(PRC._SelectNext(self.controller))
+    #         return None
+
+    #     def done(self):
+    #         return True
+
+    class _CheckWall(object):
+        def __init__(self, controller):
+            self.controller = controller
+
+        def act(self):
+            return None
+
+        def done(self):
+            return True
+
+
     class _MarkNotVisitedField(object):
         def __init__(self, controller):
             self.controller = controller
@@ -248,7 +270,7 @@ class PRC(RobotController):
             forward = controller.get_forward_position()
 
             # mark as wall
-            if controller.distance_to_obstacle < 0.6:
+            if controller.distance_to_obstacle < 1:
                 controller.times_visited[forward] = 65536
             # mark as not visited
             else:
@@ -392,3 +414,69 @@ def angle_diff(x, y):
 
 def vector_length(v):
     return math.sqrt(v[0]*v[0] + v[1] * v[1])
+
+# returns number of samples needed to estimate value to a certain certainty
+def num_samples_needed(required_certainty, error_margin, noise):
+    return int((ltqnorm((required_certainty + 1.0) / 2) * noise / error_margin) **2 + 0.5)
+
+#inverse of cumulative function of normal distribution
+def ltqnorm( p ):
+    """
+    Modified from the author's original perl code (original comments follow below)
+    by dfield@yahoo-inc.com.  May 3, 2004.
+
+    Lower tail quantile for standard normal distribution function.
+
+    This function returns an approximation of the inverse cumulative
+    standard normal distribution function.  I.e., given P, it returns
+    an approximation to the X satisfying P = Pr{Z <= X} where Z is a
+    random variable from the standard normal distribution.
+
+    The algorithm uses a minimax approximation by rational functions
+    and the result has a relative error whose absolute value is less
+    than 1.15e-9.
+
+    Author:      Peter John Acklam
+    Time-stamp:  2000-07-19 18:26:14
+    E-mail:      pjacklam@online.no
+    WWW URL:     http://home.online.no/~pjacklam
+    """
+
+    if p <= 0 or p >= 1:
+        # The original perl code exits here, we'll throw an exception instead
+        raise ValueError( "Argument to ltqnorm %f must be in open interval (0,1)" % p )
+
+    # Coefficients in rational approximations.
+    a = (-3.969683028665376e+01,  2.209460984245205e+02, \
+         -2.759285104469687e+02,  1.383577518672690e+02, \
+         -3.066479806614716e+01,  2.506628277459239e+00)
+    b = (-5.447609879822406e+01,  1.615858368580409e+02, \
+         -1.556989798598866e+02,  6.680131188771972e+01, \
+         -1.328068155288572e+01 )
+    c = (-7.784894002430293e-03, -3.223964580411365e-01, \
+         -2.400758277161838e+00, -2.549732539343734e+00, \
+          4.374664141464968e+00,  2.938163982698783e+00)
+    d = ( 7.784695709041462e-03,  3.224671290700398e-01, \
+          2.445134137142996e+00,  3.754408661907416e+00)
+
+    # Define break-points.
+    plow  = 0.02425
+    phigh = 1 - plow
+
+    # Rational approximation for lower region:
+    if p < plow:
+       q  = math.sqrt(-2*math.log(p))
+       return (((((c[0]*q+c[1])*q+c[2])*q+c[3])*q+c[4])*q+c[5]) / \
+               ((((d[0]*q+d[1])*q+d[2])*q+d[3])*q+1)
+
+    # Rational approximation for upper region:
+    if phigh < p:
+       q  = math.sqrt(-2*math.log(1-p))
+       return -(((((c[0]*q+c[1])*q+c[2])*q+c[3])*q+c[4])*q+c[5]) / \
+                ((((d[0]*q+d[1])*q+d[2])*q+d[3])*q+1)
+
+    # Rational approximation for central region:
+    q = p - 0.5
+    r = q*q
+    return (((((a[0]*r+a[1])*r+a[2])*r+a[3])*r+a[4])*r+a[5])*q / \
+           (((((b[0]*r+b[1])*r+b[2])*r+b[3])*r+b[4])*r+1)
