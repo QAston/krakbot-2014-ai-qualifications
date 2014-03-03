d63282b393f9a1b9ce1cd07b495a23e26194a81f
dam3r <dam3r@gmail.com>
2014-03-03 23:49:21 +0100

    Pierwsza wersja kretyna

--- robots/kretyn.py
+++ robots/kretyn.py
@@ -1,3 +1,4 @@
+
 from defines import *
 from robot_controller import RobotController
 import random
@@ -7,10 +8,8 @@ class OmitCollisions(RobotController):
     STATE_FORWARD = 0
     STATE_LOOK_FOR_SPACE = 1
 
-    STRONA_LEWA = 0
-    STRONA_PRAWA = 0
-
     def init(self, starting_position, steering_noise, distance_noise, sonar_noise, measurement_noise, speed, turning_speed,gps_delay,execution_cpu_time_limit):
+
 
         self.phase = OmitCollisions.STATE_LOOK_FOR_SPACE
         self.speed = speed
@@ -23,16 +22,17 @@ class OmitCollisions(RobotController):
     def act(self):
         if len(self.command_queue) == 0:
             if self.phase == OmitCollisions.STATE_LOOK_FOR_SPACE:
+                self.command_queue.append([TURN, int((pi/100.0) / TICK_ROTATE )]) # Rotate by small angle
                 self.command_queue.append([SENSE_SONAR])
                 self.command_queue.append([SENSE_GPS])
-                self.command_queue.append([WRITE_CONSOLE, "Przeszkoda wykryta: "+str(self.x)+" "+str(self.y)])
+                self.command_queue.append([WRITE_CONSOLE, "OmitCollisions bot reporting with "+str(self.x)+" "+str(self.y)])
             elif self.phase == MAP_GOAL:
                 self.command_queue.append([FINISH])
             else:
-                self.command_queue.append([TURN, -10])
-                self.command_queue.append([MOVE, 1])
+                self.command_queue.append([MOVE, 0.002])
                 self.command_queue.append([SENSE_SONAR])
                 self.command_queue.append([SENSE_FIELD])
+
         return self.command_queue.pop(0)
 
     def on_sense_gps(self, x, y):
@@ -41,9 +41,8 @@ class OmitCollisions(RobotController):
 
     def on_sense_sonar(self, distance):
         self.last_distance = distance
-        if distance < 0.2:
-            self.phase == OmitCollisions.STATE_LOOK_FOR_SPACE
-            self.command_queue.append([TURN, 300])
+        if distance < 0.315/distance:
+            self.phase = OmitCollisions.STATE_LOOK_FOR_SPACE
         else:
             self.phase = OmitCollisions.STATE_FORWARD
 
