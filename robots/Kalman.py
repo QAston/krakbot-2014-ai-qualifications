a0940db03d97cc24a4d23b42c826b893639728d9
Khanrad <mgkonrad@gmail.com>
2014-03-08 20:50:45 +0100

    kalman zmiany

--- robots/Kalman.py
+++ robots/Kalman.py
@@ -69,6 +69,7 @@ class KalmanFilter(RobotController):
 
 
     def act(self):
+        print("act")
         if len(self.command_queue) == 0:
 
             if self.phase == KalmanFilter.STATE_MOTION:
@@ -79,40 +80,68 @@ class KalmanFilter(RobotController):
                 self.command_queue.append([FINISH])
 
             elif self.phase == KalmanFilter.STATE_DECIDE_NEXT:
-                self.phase_helper = 0
+                print("decide")
                 self.mapa[(self.actual_x, self.actual_y)] = 1
                 neigh = [(self.actual_x + 1, self.actual_y), (self.actual_x, self.actual_y - 1),
                          (self.actual_x - 1, self.actual_y), (self.actual_x, self.actual_y + 1)]
                 if not all((x in self.mapa for x in neigh)):
                     self.command_queue.append([SENSE_SONAR])
                     self.phase = KalmanFilter.STATE_SCANNING
-
-        elif self.phase == KalmanFilter.STATE_SCANNING:
-
-            # Directional cosinus
-            # Change of coordinaes for convenience
-            vector = (math.cos((self.actual_angle - 90.0) / 180.0 * math.pi),
-                      math.sin((self.actual_angle - 90.0) / 180.0 * math.pi))
-            scanned = (self.actual_x - round(vector[1]), self.actual_y + round(vector[0]))
-
-            if (self.actual_x - round(vector[1] + 0.01), self.actual_y + round(vector[0] + 0.01)) not in self.mapa:
-                if self.last_distance < 0.9:
-                    # Strange indexing because x runs vertically and y runs horizontally
-                    # Set big number so that it won't be visited
-                    self.mapa[(self.actual_x - round(vector[1] + 0.01), self.actual_y + round(vector[0] + 0.01))] = 1000
+                    print("tu skanuje")
                 else:
-                    self.mapa[(self.actual_x - round(vector[1] + 0.01), self.actual_y + round(vector[0] + 0.01))] = 0
+                    print("elo jestem tu!")
+                    neigh = [(self.actual_x + 1, self.actual_y), (self.actual_x, self.actual_y - 1),
+                         (self.actual_x - 1, self.actual_y), (self.actual_x, self.actual_y + 1)]
+                    find_min = [(self.mapa[x], x) for x in neigh]
+                    find_min.sort()
+                    print "Result of scanning, or known before: ", find_min
 
-            self.phase_helper += 1
+                    goal = find_min[0][1] # Get minimum goal
+                    print "Goal is ", goal
+                    vector = (goal[0] - self.actual_x, goal[1] - self.actual_y)
+                    print "Vector is ", vector
+                    # Angle calculation + change of coordinates
+                    angle = math.atan2(-vector[0], vector[1]) / math.pi * 180.0 + 90.0
 
 
-            # It means that we have reached the last rotation
-            if self.phase_helper == 4:
-                self.phase = KalmanFilter.STATE_DECIDE_NEXT
-            else:
-                self.command_queue.append([TURN, int(0.5 * math.pi / TICK_ROTATE)])
-                self.angle = (self.angle + 90.0) % 360
-                self.command_queue.append([SENSE_SONAR])
+
+
+                    print "Current angle is ",self.actual_angle, "rotation by ",(angle - self.angle), " to ",angle
+                    rotation_ticks = int((angle - self.actual_angle)/180.0 * math.pi / TICK_ROTATE)
+                    if rotation_ticks != 0 : self.command_queue.append([TURN, rotation_ticks])
+                    self.actual_angle = angle
+                    self.command_queue.append([MOVE, int(1.0/TICK_MOVE)])
+                    self.phase = KalmanFilter.STATE_MEASURMENTS
+
+                self.state_helper = 0
+            elif self.phase == KalmanFilter.STATE_SCANNING:
+                print("scanniing")
+                # Directional cosinus
+                # Change of coordinaes for convenience
+                vector = (math.cos((self.actual_angle - 90.0) / 180.0 * math.pi),
+                          math.sin((self.actual_angle - 90.0) / 180.0 * math.pi))
+                scanned = (self.actual_x - round(vector[1]), self.actual_y + round(vector[0]))
+
+                if (self.actual_x - round(vector[1] + 0.01), self.actual_y + round(vector[0] + 0.01)) not in self.mapa:
+                    if self.last_distance < 0.9:
+                        # Strange indexing because x runs vertically and y runs horizontally
+                        # Set big number so that it won't be visited
+                        self.mapa[(self.actual_x - round(vector[1] + 0.01), self.actual_y + round(vector[0] + 0.01))] = 1000
+                    else:
+                        self.mapa[(self.actual_x - round(vector[1] + 0.01), self.actual_y + round(vector[0] + 0.01))] = 0
+
+                self.phase_helper += 1
+
+
+                # It means that we have reached the last rotation
+                if self.phase_helper == 4:
+                    self.phase = KalmanFilter.STATE_DECIDE_NEXT
+                    print("tu powinno wrocic")
+                    print(len(self.command_queue))#tu trzeba cos dodac bo on chce pobrac a nie ma z czego
+                else:
+                    self.command_queue.append([TURN, int(0.5 * math.pi / TICK_ROTATE)])
+                    self.actual_angle = (self.actual_angle + 90.0) % 360
+                    self.command_queue.append([SENSE_SONAR])
 
         return self.command_queue.pop(0)
 
