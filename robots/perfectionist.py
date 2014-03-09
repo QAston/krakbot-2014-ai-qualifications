ce127b03101b48c081297acc66ce3147ad023260
Khanrad <mgkonrad@gmail.com>
2014-03-09 21:23:27 +0100

    kalman do2lączony do zbieranie odczytów GPS

--- robots/perfectionist.py
+++ robots/perfectionist.py
@@ -13,6 +13,139 @@ import numpy as np
 from numpy import matlib
 from scipy import stats
 
+class matrix:
+
+    # implements basic operations of a matrix class
+
+    def __init__(self, value):
+        self.value = value
+        self.dimx = len(value)
+        self.dimy = len(value[0])
+        if value == [[]]:
+            self.dimx = 0
+
+    def zero(self, dimx, dimy):
+        # check if valid dimensions
+        if dimx < 1 or dimy < 1:
+            raise ValueError, "Invalid size of matrix"
+        else:
+            self.dimx = dimx
+            self.dimy = dimy
+            self.value = [[0 for row in range(dimy)] for col in range(dimx)]
+
+    def identity(self, dim):
+        # check if valid dimension
+        if dim < 1:
+            raise ValueError, "Invalid size of matrix"
+        else:
+            self.dimx = dim
+            self.dimy = dim
+            self.value = [[0 for row in range(dim)] for col in range(dim)]
+            for i in range(dim):
+                self.value[i][i] = 1
+
+    def show(self):
+        for i in range(self.dimx):
+            print self.value[i]
+        print ' '
+
+    def __add__(self, other):
+        # check if correct dimensions
+        if self.dimx != other.dimx or self.dimy != other.dimy:
+            raise ValueError, "Matrices must be of equal dimensions to add"
+        else:
+            # add if correct dimensions
+            res = matrix([[]])
+            res.zero(self.dimx, self.dimy)
+            for i in range(self.dimx):
+                for j in range(self.dimy):
+                    res.value[i][j] = self.value[i][j] + other.value[i][j]
+            return res
+
+    def __sub__(self, other):
+        # check if correct dimensions
+        if self.dimx != other.dimx or self.dimy != other.dimy:
+            raise ValueError, "Matrices must be of equal dimensions to subtract"
+        else:
+            # subtract if correct dimensions
+            res = matrix([[]])
+            res.zero(self.dimx, self.dimy)
+            for i in range(self.dimx):
+                for j in range(self.dimy):
+                    res.value[i][j] = self.value[i][j] - other.value[i][j]
+            return res
+
+    def __mul__(self, other):
+        # check if correct dimensions
+        if self.dimy != other.dimx:
+            raise ValueError, "Matrices must be m*n and n*p to multiply"
+        else:
+            # subtract if correct dimensions
+            res = matrix([[]])
+            res.zero(self.dimx, other.dimy)
+            for i in range(self.dimx):
+                for j in range(other.dimy):
+                    for k in range(self.dimy):
+                        res.value[i][j] += self.value[i][k] * other.value[k][j]
+            return res
+
+    def transpose(self):
+        # compute transpose
+        res = matrix([[]])
+        res.zero(self.dimy, self.dimx)
+        for i in range(self.dimx):
+            for j in range(self.dimy):
+                res.value[j][i] = self.value[i][j]
+        return res
+
+    # Thanks to Ernesto P. Adorio for use of Cholesky and CholeskyInverse functions
+
+    def Cholesky(self, ztol=1.0e-5):
+        # Computes the upper triangular Cholesky factorization of
+        # a positive definite matrix.
+        res = matrix([[]])
+        res.zero(self.dimx, self.dimx)
+
+        for i in range(self.dimx):
+            S = sum([(res.value[k][i])**2 for k in range(i)])
+            d = self.value[i][i] - S
+            if abs(d) < ztol:
+                res.value[i][i] = 0.0
+            else:
+                if d < 0.0:
+                    raise ValueError, "Matrix not positive-definite"
+                res.value[i][i] = math.sqrt(d)
+            for j in range(i+1, self.dimx):
+                S = sum([res.value[k][i] * res.value[k][j] for k in range(self.dimx)])
+                if abs(S) < ztol:
+                    S = 0.0
+                res.value[i][j] = (self.value[i][j] - S)/res.value[i][i]
+        return res
+
+    def CholeskyInverse(self):
+        # Computes inverse of matrix given its Cholesky upper Triangular
+        # decomposition of matrix.
+        res = matrix([[]])
+        res.zero(self.dimx, self.dimx)
+
+        # Backward step for inverse.
+        for j in reversed(range(self.dimx)):
+            tjj = self.value[j][j]
+            S = sum([self.value[j][k]*res.value[j][k] for k in range(j+1, self.dimx)])
+            res.value[j][j] = 1.0/tjj**2 - S/tjj
+            for i in reversed(range(j)):
+                res.value[j][i] = res.value[i][j] = -sum([self.value[i][k]*res.value[k][j] for k in range(i+1, self.dimx)])/self.value[i][i]
+        return res
+
+    def inverse(self):
+        aux = self.Cholesky()
+        res = aux.CholeskyInverse()
+        return res
+
+    def __repr__(self):
+        return repr(self.value)
+    def __getitem__(self, item):
+        return self.value[item]
 
 class PRC(RobotController):
     DISTANCE_PRECISION_LOW = 2
@@ -77,6 +210,37 @@ class PRC(RobotController):
         self.sonar_cache = []
 
         self.change_ai(PRC.AI_PERFECT)
+
+        #kalman matrix
+        self.dt = (1.0/TICK_MOVE) * TICK_MOVE/speed
+        self.measurements = [0.,0.]
+        self.X = matrix([[starting_position[0]], [starting_position[1]], [distance_noise], [distance_noise]])
+        self.u = matrix([[0.], [0.], [0.], [0.]])
+        self.F = matrix([[1., 0., self.dt, 0.], [0, 1., 0., self.dt], [0., 0., 1., 0.], [0., 0., 0., 1.]])
+        self.H = matrix([[1., 0., 0., 0.], [0., 1., 0., 0.]])
+        self.P = matrix(
+            [[0., 0., 0., 0.], [0., 0., 0., 0.], [0., 0., distance_noise, 0.], [0., 0., 0., distance_noise]])
+        self.R = matrix([[gps_noise, 0.0], [0.0, gps_noise]])
+        self.I = matrix([[1., 0., 0., 0.], [0., 1., 0., 0.], [0., 0., 1., 0.], [0., 0., 0., 1.]])
+
+    def filter(self, x, P):
+
+        x = (self.F * x) + self.u
+        P = self.F * P * self.F.transpose()
+
+        Z = matrix([[self.measurements[0]],[self.measurements[1]]])
+        y = Z - (self.H * x)
+        S = self.H * P * self.H.transpose() + self.R
+        K = P * self.H.transpose() * S.inverse()
+        x = x + (K * y)
+        P = (self.I - (K * self.H)) * P
+        #result
+        print 'x= '
+        x.show()
+        self.X = x
+        print 'P= '
+        P.show()
+        self.P = P
 
     # changes ai to selected one, or dumber if selected can't be used
     def change_ai(self, ai):
@@ -431,6 +595,9 @@ class PRC(RobotController):
 
 
         def done(self):
+            controller = self.controller
+            controller.X = matrix([[controller.theoretical_position[0]],[controller.theoretical_position[1]],[controller.distance_noise],[controller.distance_noise]])
+            print "------------------zaaktualizowalo macierz--------------------"
             return True
 
     #low level function, use other ones instead
@@ -440,6 +607,7 @@ class PRC(RobotController):
             self.controller = controller
 
         def act(self):
+
             controller = self.controller
             #print "MoveTicks {} ticks: {}".format(str(controller), self.move_times)
             #simulate perfect movement, don't account for randomness as result will be different from robot's rand anyways
@@ -494,6 +662,14 @@ class PRC(RobotController):
 
                 #print "UpdateMPosWithGps: samples {}, move_pos{}".format(num_samples, controller.movement_position)
                 return True
+            else:
+                controller.measurements = controller.last_gps_read
+                controller.filter(controller.X, controller.P)
+                new_x = controller.X[0]
+                new_y = controller.X[1]
+                controller.movement_position = int(new_x[0]), int(new_y[0])
+                return True
+
             return None
 
     class _UpdateMovementPositionWithSonar(object):
@@ -819,7 +995,6 @@ class PRC(RobotController):
 
             c.append(self)
             return True
-
 
 def get_front(pos, angle):
     dist = 1.0
